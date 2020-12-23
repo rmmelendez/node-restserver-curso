aa21bf6ad35726e51f7a44298973efe35a062168
Mauricio Melendez <m_melendez2002@outlook.com>
2020-12-22 18:24:03 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://rapiduser:tVwjaHQrjKnUGHxA@cluster0.85mba.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
